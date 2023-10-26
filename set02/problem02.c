146a267f6cae580cc66ad8183779e35e98f0bd65
Anagha RJ <anagharj7@gmail.com>
2023-10-26 19:47:55 +0530

    committed 10/26/2023, 7:47:54 PM

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 int input_side();
-int check_scalene(int a, int b, int c);
+int check_scalene(int a, int b, int c);P
 void output(int a, int b, int c, int isscalene);
 
-int main()
-{
-    int a=0;
 
 }
 int input_side()
 {
-    printf("Enter the length of side 1")
-}
+    printf("Enter the length of side 1");
+    scanf("")}
